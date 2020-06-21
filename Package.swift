7f34ee61b5a44f342f710051f54a32a5710f8eae
Zack Elia <github@zacharyelia.com>
2020-06-21 19:59:06 -0400

    Implement BCLM

--- Package.swift
+++ Package.swift
@@ -6,15 +6,14 @@ import PackageDescription
 let package = Package(
     name: "bclm",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "bclm",
-            dependencies: []),
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
         .testTarget(
             name: "bclmTests",
             dependencies: ["bclm"]),
--- Sources/bclm/main.swift
+++ Sources/bclm/main.swift
@@ -1,1 +1,73 @@
-print("Hello, world!")
+import ArgumentParser
+import Foundation
+
+struct BCLM: ParsableCommand {
+    static let configuration = CommandConfiguration(
+            abstract: "Battery Charge Level Max (BCLM) Utility.",
+            subcommands: [Read.self, Write.self])
+
+    struct Read: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Reads the BCLM value.")
+
+        func run() {
+            do {
+                try SMCKit.open()
+            } catch {
+                print(error)
+            }
+
+            let key = SMCKit.getKey("BCLM", type: DataTypes.UInt8)
+            do {
+                let status = try SMCKit.readData(key).0
+                print(status)
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    struct Write: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            abstract: "Writes a BCLM value.")
+
+        @Argument(help: "The value to set (50-100)")
+        var value: Int
+
+        func validate() throws {
+            guard getuid() == 0 else {
+                throw ValidationError("Must run as root.")
+            }
+
+            guard value >= 50 && value <= 100 else {
+                throw ValidationError("Value must be between 50 and 100.")
+            }
+        }
+
+        func run() {
+            do {
+                try SMCKit.open()
+            } catch {
+                print(error)
+            }
+
+            let key = SMCKit.getKey("BCLM", type: DataTypes.UInt8)
+
+            let bytes: SMCBytes = (
+                UInt8(value), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0),
+                UInt8(0), UInt8(0), UInt8(0), UInt8(0), UInt8(0)
+            )
+
+            do {
+                try SMCKit.writeData(key, data: bytes)
+            } catch {
+                print(error)
+            }
+        }
+    }
+}
+
+BCLM.main()
