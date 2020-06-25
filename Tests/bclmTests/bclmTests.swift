ded21ea492c62ae4ad74263ef6e7066c582c9e06
Zack Elia <github@zacharyelia.com>
2020-06-25 19:51:46 -0400

    Add tests

--- Tests/bclmTests/bclmTests.swift
+++ Tests/bclmTests/bclmTests.swift
@@ -2,31 +2,78 @@ import XCTest
 import class Foundation.Bundle
 
 final class bclmTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
 
+    /// Helper method to run bclm
+    func bclm(args: String...) -> String! {
         // Some of the APIs that we use below are available in macOS 10.13 and above.
         guard #available(macOS 10.13, *) else {
-            return
+            XCTFail("macOS version >= 10.13 required to run tests.")
+            return ""
         }
 
-        let fooBinary = productsDirectory.appendingPathComponent("bclm")
-
+        let binary = self.productsDirectory.appendingPathComponent("bclm")
         let process = Process()
-        process.executableURL = fooBinary
-
         let pipe = Pipe()
-        process.standardOutput = pipe
 
-        try process.run()
+        process.executableURL = binary
+        process.arguments = args
+        process.standardOutput = pipe
+        process.standardError = pipe
+
+        do {
+            try process.run()
+        }
+        catch {
+            XCTFail("Could not start bclm process.")
+        }
+
         process.waitUntilExit()
 
         let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
+        let output = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines)
 
-        XCTAssertEqual(output, "Hello, world!\n")
+        return output
+    }
+
+    /// Helper method to run bclm read
+    func readBCLM() -> String!  {
+        return bclm(args: "read")
+    }
+
+    /// Helper method to run bclm write
+    func writeBCLM(value: Int) -> String!  {
+        return bclm(args: "write", String(value))
+    }
+
+    /// Verify that reading the bclm returns a value
+    func testRead() {
+        var bclm: Int!
+
+        bclm = Int(readBCLM()!)!
+        XCTAssertNotNil(bclm)
+    }
+
+    /// Verify that writing a valid bclm value works
+    func testWriteValid() {
+        var bclm: Int!
+        var output: String!
+
+        // Get the current value to not mess up the runner's configuration
+        bclm = Int(readBCLM()!)!
+
+        output = writeBCLM(value: bclm)!
+        XCTAssertEqual(output, "")
+    }
+
+    // Verify that writing an invalid bclm value did not work
+    func testWriteInvalid() throws {
+        var output: String!
+
+        output = writeBCLM(value: 101)!
+        XCTAssertNotEqual(output, "")
+
+        output = writeBCLM(value: 0)!
+        XCTAssertNotEqual(output, "")
     }
 
     /// Returns path to the built products directory.
@@ -42,6 +89,8 @@ final class bclmTests: XCTestCase {
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testRead", testRead),
+        ("testWriteValid", testWriteValid),
+        ("testWriteInvalid", testWriteInvalid),
     ]
 }
